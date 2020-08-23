9a7cd01c09c3763977db79aa31338e4c5ee98877
haj8110 <haj8110@gmail.com>
2020-08-23 10:53:31 +0530

    calender

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', 'UrlController@index')->name('index');
+Route::get('/clock', 'UrlController@clock')->name('clock');
